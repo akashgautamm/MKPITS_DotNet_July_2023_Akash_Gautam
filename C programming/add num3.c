6ef12e3cf748ec61dc1cf9a5beaf58ccd68be8cd
Akash Gautam <gautama55696@gmail.com>
2023-07-13 18:07:45 +0530

    c program

---
+++ C programming/add num3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+	int num1,num2,num3,add;
+	printf("Enter digit 1");
+	scanf("%d",&num1);
+	printf("Enter digit 2");
+	scanf("%d",&num2);
+	printf("Enter digit 3");
+	scanf("%d",&num3);
+	add=num1+num2+num3;
+	printf("add=%d",add);
+}
---
+++ C programming/cube.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main()
+
+{
+	int cube,num;
+	printf("Enter any number");
+	scanf("%d",&num);
+	cube=num*num*num;
+	printf("cube=%d",cube);
+}
