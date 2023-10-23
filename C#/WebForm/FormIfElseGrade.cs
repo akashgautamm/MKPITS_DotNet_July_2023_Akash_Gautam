f298c78b0d3da14ad8663b287cc4c61bce527789
Akash Gautam <gautama55696@gmail.com>
2023-10-23 16:56:15 +0530

    Web Form

---
+++ C#/WebForm/FormIfElseGrade.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormIfElseGrade
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            char op;
+            op = Convert.ToChar(textBox1.Text);
+            if (op == 'E')
+                label2.Text = "Excellent";
+            else if (op == 'V')
+                label2.Text = "Very Good";
+            else if (op == 'G')
+                label2.Text = "Good";
+            else if (op == 'A')
+                label2.Text = "Average";
+            else if (op == 'F')
+                label2.Text = "Fail";
+            else
+                label2.Text = "Invalid grade";
+
+
+
+        }
+    }
+}
---
+++ C#/WebForm/FormIfElseNum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormIfElseNum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num;
+            num = Convert.ToInt32(textBox1.Text);
+
+            if (num == 1)
+                label2.Text = "One";
+            else if (num == 2)
+                label2.Text = "Two";
+            else if (num == 3)
+                label2.Text = "Three";
+            else if (num == 4)
+                label2.Text = "Four";
+            else if (num == 5)
+                label2.Text = "Five";
+            else if (num == 6)
+                label2.Text = "Six";
+            else if (num == 7)
+                label2.Text = "Seven";
+            else
+                label2.Text = "Invalid day No";
+        }
+    }
+}
