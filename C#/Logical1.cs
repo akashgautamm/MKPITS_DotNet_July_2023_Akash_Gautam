bb3ccddfc3763052eb855cf71ee895edb9c3a697
Akash Gautam <gautama55696@gmail.com>
2023-10-24 20:02:45 +0530

    C#

---
+++ C#/Logical1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 2;
+            int j = 10 / 4;
+            if(i==j)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+            Console.ReadKey();
+        }
+    }
+}
