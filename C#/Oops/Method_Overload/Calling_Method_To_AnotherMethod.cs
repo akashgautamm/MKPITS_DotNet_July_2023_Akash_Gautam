89d8e582f90fc5aab474035cf9b7addbed7db7dd
Akash Gautam <gautama55696@gmail.com>
2023-11-06 18:25:57 +0530

    C# oops

---
+++ C#/Oops/Method_Overload/Calling_Method_To_AnotherMethod.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overload_Callingone_method_Anothermethod
+{
+    internal class Program
+    {
+        class Book
+        {
+            int number;
+            string Name;
+            
+            public void getdata(int number)
+            {
+                getdata(number, "Akash");
+            }
+            public void getdata(int number, string name)
+            {
+                this.number = number;
+                this.Name = name;
+            }
+            public void display()
+            {
+                Console.WriteLine("Number: " + number);
+                Console.WriteLine("Name: " + Name);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            book.getdata(123);
+            book.display();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Oops/Method_Overload/Method_Overload_Book4Parameter.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overload_Book4Parameter
+{
+    internal class Program
+    {
+        class Book
+        {
+            int Price;
+            string Name;
+            string Title;
+            string Author;
+            public void getdata()
+            {
+                Price = 250;
+                Name = "John";
+                Title = "Jungle Book";
+                Author = "Rest";
+            }
+            public void getdata(int price)
+            {
+                this.Price = price;
+            }
+            public void getdata(int price,string name)
+            {
+                this.Price = price;
+                this.Name = name;
+            }
+            public void getdata(int price, string name,string title)
+            {
+                this.Price = price;
+                this.Name = name;
+                this.Title = title;
+            }
+            public void getdata(int price, string name, string title,string author)
+            {
+                this.Price = price;
+                this.Name = name;
+                this.Title = title;
+                this.Author = author;
+            }
+            public void display()
+            {
+                Console.WriteLine("Book Price: " + Price);
+                Console.WriteLine("Book Name: " + Name);
+                Console.WriteLine("Book Title: " + Title);
+                Console.WriteLine("Book Author: " + Author);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Book book0 = new Book();
+            book0.getdata();
+            book0.display();
+            Book book1 = new Book();
+            book1.getdata(100);
+            book1.display();
+            Book book2=new Book();
+            book2.getdata(200, "John");
+            book2.display();
+            Book book3 = new Book();
+            book3.getdata(200, "John","Magic Book");
+            book3.display();
+            Book book4 = new Book();
+            book4.getdata(200, "John", "Magic Book","Vishal");
+            book4.display();
+
+            Console.ReadKey();
+
+        }
+    }
+}
