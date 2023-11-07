6c71b255b0ad889c61c14f8b9ae4348a4b85b4b9
Akash Gautam <gautama55696@gmail.com>
2023-11-07 22:50:37 +0530

    C# oops

---
+++ C#/Oops/Method_Overload/Destructor_Employe.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Destructor_Employe
+{
+    internal class Program
+    {
+        class Employe
+        {
+            public Employe()
+            {
+                Console.WriteLine("Constructor Called");
+            }
+            ~Employe()
+            {
+                Console.WriteLine("Destructor Called");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Employe emp = new Employe();
+            Console.ReadKey();
+            Employe emp1 = new Employe();
+            Console.ReadKey();
+        }
+    }
+}
