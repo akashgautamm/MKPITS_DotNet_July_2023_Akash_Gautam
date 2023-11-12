5cb5016b4eb2fd1d386138e5adb24f97d1bd7c58
Akash Gautam <gautama55696@gmail.com>
2023-11-12 22:19:53 +0530

    C# oops

---
+++ C#/ArrayMaxMin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayMax_Min
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            int max = 0;
+            int min = 0;
+            for (int cnt=0;cnt<5;cnt++)
+            {
+                arr[cnt] = Convert.ToInt32(Console.ReadLine());
+            }
+            max = arr[0];
+            for(int cnt=1;cnt<5;cnt++)
+            {
+                if (arr[cnt]>max)
+                {
+                    max = arr[cnt];
+
+                }
+            }
+            min = arr[0];
+            for (int cnt = 0; cnt < 5; cnt++)
+            {
+                if (arr[cnt]<min)
+                {
+                    min= arr[cnt];
+                }
+            }
+            Console.WriteLine("Maximum is " + max);
+            Console.WriteLine("Mainimum is " + min);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Oops/Constructor/Constructor_Static_Cube.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Maths
+{
+    class Maths
+    {
+        public static float p=3.14f;
+        public static int cube(int n)
+        {
+            return n * n * n;
+        }
+             
+     }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Value of PI is: " + Maths.p);
+            Console.WriteLine("Cube of 3 is: " + Maths.cube(3));
+            Console.ReadKey();
+
+        }
+    }
+}
