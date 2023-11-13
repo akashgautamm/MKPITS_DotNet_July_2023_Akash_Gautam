1f09dc3437692312d73cdb5c7ab5645cb74254f6
Akash Gautam <gautama55696@gmail.com>
2023-11-13 19:50:53 +0530

    C# oops

---
+++ C#/ArraySecondSmallest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySecondLargest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            int min1, min2;
+            for (int cnt = 0; cnt < 5; cnt++)
+            {
+                arr[cnt] = Convert.ToInt32(Console.ReadLine());
+            }
+            min1 = min2 = arr[0];
+            for (int cnt = 0; cnt < 5; cnt++)
+            {
+                if (arr[cnt] < min1)
+                {
+                    min2 = min1;
+                    min1 = arr[cnt];
+                }
+                else if (arr[cnt] < min2)
+                {
+                    min2 = arr[cnt];
+                }
+            }
+            Console.WriteLine("First minimum is " + min1);
+            Console.WriteLine("Second minimum is " + min2);
+
+            Console.ReadKey();
+        }
+    }
+}
