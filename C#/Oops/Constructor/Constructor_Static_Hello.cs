007afb161a87518070e2324c653c4b37f1b7f01e
Akash Gautam <gautama55696@gmail.com>
2023-11-14 23:02:10 +0530

    C# oops

---
+++ C#/Oops/Constructor/Constructor_Static_Hello.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Static_Hello
+{
+    internal class Program
+    {
+        class Hello
+        {
+            public static void display()
+            {
+                Console.WriteLine("Hello from Static");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Hello.display();
+            Console.ReadKey();
+        }
+    }
+}
