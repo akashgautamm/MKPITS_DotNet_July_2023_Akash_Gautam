51d5533404ad235bda2c4b43b45f849504ab0d12
Akash Gautam <gautama55696@gmail.com>
2023-11-18 21:51:27 +0530

    C Sharp

---
+++ C#/Oops/Polymorphism/Polymorphism_Example.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Example
+{
+    class Base
+    {
+        public virtual void display()
+        {
+            Console.WriteLine("Hello from base class");
+        }
+    }
+    class Child:Base
+    {
+        public override void display()
+        {
+            Console.WriteLine("Hello from child class");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Base b = new Child();
+            b.display();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Oops/Polymorphism/Polymorphism_Shape.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Shape
+{
+    class Shape
+    {
+        public virtual void draw()
+        {
+            Console.WriteLine("Drawing.....");
+        }
+    }
+    class Rectangle:Shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("Draw Rectangle.....");
+        }
+    }
+    class Circle:Shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("Draw Circle..... ");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape s;
+            s = new Shape();
+            s.draw();
+            s = new Rectangle();
+            s.draw();
+            s = new Circle();
+            s.draw();
+            Console.ReadKey();
+        }
+    }
+}
