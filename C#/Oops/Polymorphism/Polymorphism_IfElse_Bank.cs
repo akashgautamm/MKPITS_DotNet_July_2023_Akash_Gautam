8dc04e4df3807c2dd8acb9e08e5fc744d8b37011
Akash Gautam <gautama55696@gmail.com>
2023-11-19 23:36:53 +0530

    C Sharp

---
+++ C#/Oops/Polymorphism/Polymorphism_IfElse_Bank.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Ifelse_Bank
+{
+    class Account
+    {
+        public int Account_Number;
+        public int Balance = 1000;
+        public virtual void deposit(int Account_Number,int Amount)
+        {
+            Console.WriteLine("Deposite Amount of method class");
+        }
+        public void showbalance()
+        {
+            Console.WriteLine("Account Number: " + Account_Number);
+            Console.WriteLine("Balance is: " + Balance);
+        }
+    }
+    class Current:Account
+    {
+        public override void deposit(int Account_Number, int Amount)
+        {
+            this.Account_Number = Account_Number;
+            Balance = Balance + Amount;
+            Console.WriteLine("Deposit amount successful in current account");
+        }
+    }
+    class Saving : Account
+    {
+        public override void deposit(int Account_Number, int Amount)
+        {
+            int Intrest = 500;
+            this.Account_Number = Account_Number;
+            Balance = Balance + Amount+Intrest;
+            Console.WriteLine("Deposit amount successful in Saving account");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Account Number: ");
+            int actno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Deposit Amount: ");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Saving or Current");
+            string choice = Console.ReadLine();
+            Account act = null;
+            if(choice =="Current")
+            {
+                act=new Current();
+            }
+            else if(choice =="Saving")
+            {
+                act=new Saving();
+            }
+            act.deposit(actno,amount);
+            act.showbalance();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Oops/Polymorphism/Runtime_Polymorphism_Bank.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Runtime_Polymorphism_Bank
+{
+    class Account
+    {
+        public int actno;
+        public int balance = 1000;
+        public  virtual void deposit(int actno,int amt)
+        {
+            Console.WriteLine("Deposite method of account class");
+        }
+        public void showbalance()
+        {
+            Console.WriteLine("Account Number: " + actno);
+            Console.WriteLine("Balance is : " + balance);
+            Console.WriteLine("-------------------------------");
+        }
+    }
+    class Current:Account
+    {
+       
+        public override void deposit(int actno, int amt)
+        {
+            this.actno = actno;
+            
+            balance = amt + balance;
+            Console.WriteLine("Amount deposite successful");
+        }
+        
+    }
+    class Saving:Account
+    {
+       
+        public override void deposit(int actno, int amt)
+        {
+            int intrest = 500;
+            this.actno = actno;
+         
+            balance = amt + balance+intrest;
+            Console.WriteLine("Saving amount deposite successful");
+            
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account act = new Current();
+            Console.WriteLine("Enter Account Num:");
+            int actno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Amount Deposit: ");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            act.deposit(actno, amount);
+            act.showbalance();
+
+            Account act1 = new Saving();
+            Console.WriteLine("Enter Account Num: ");
+            int actno1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Amount Deposit: ");
+            int amount1= Convert.ToInt32(Console.ReadLine());
+            act1.deposit(actno1, amount1);
+            act1.showbalance();
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
