7312417674ea3006a541753ea27f6dc9683d19c0
Akash Gautam <gautama55696@gmail.com>
2023-11-25 21:57:37 +0530

    C Sharp

---
+++ C#/Oops/Array_List/Queue_Method.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Queue_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+            q.Enqueue('A');
+            q.Enqueue('B');
+            q.Enqueue('C');
+            q.Enqueue('D');
+            Console.WriteLine("Current Enqueue");
+            foreach(char c in q)
+                Console.WriteLine(c);
+            Console.WriteLine("Removing Some Value");
+            char ch = (char)q.Dequeue();
+             ch = (char)q.Dequeue();
+            Console.WriteLine(ch);
+            Console.WriteLine("After Dequeue");
+            foreach (char c in q)
+                Console.WriteLine(c);
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
---
+++ C#/Oops/Inheritance/Example_Polymorphism.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class Person
+    {
+        public string name;
+        public string address;
+    }
+    class Employe:Person
+    {
+        public int id;
+        public int salary;
+    }
+    class Part:Employe
+    {
+        public int hrs;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Part p = new Part();
+            p.name = "Akash";
+            p.address = "Nagpur";
+            p.id = 1;
+            p.salary = 25000;
+            p.hrs = 1;
+            Console.WriteLine(p.name);
+            Console.WriteLine(p.address);
+            Console.WriteLine(p.id);
+            Console.WriteLine(p.salary);
+            Console.WriteLine(p.hrs);
+            Console.ReadKey();
+        }
+    }
+}
