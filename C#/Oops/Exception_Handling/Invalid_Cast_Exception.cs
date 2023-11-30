1727f719739635ca2decfa68290580216de347f3
Akash Gautam <gautama55696@gmail.com>
2023-11-30 23:31:38 +0530

    C Sharp

---
+++ C#/Oops/Exception_Handling/Invalid_Cast_Exception.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invalid_Cast_Exception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Amit");
+            sb.Append("Shreya");
+            Console.WriteLine("sb="+sb.ToString());
+            Object ob = sb;
+            Console.WriteLine("Ob="+ob.ToString());
+            try
+            {
+                System.IO.StreamReader ss = (System.IO.StreamReader)ob;
+            }
+            catch(InvalidCastException ee)
+            {
+                Console.WriteLine(ee.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Oops/Exception_Handling/Out_Memory_Exception.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Out_Memory_Exception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Akash";
+            string subject = "Javascript";
+            Console.WriteLine("Name: " + name.Length);
+            StringBuilder sb = new StringBuilder(name.Length,name.Length);
+            try
+            {
+                sb.Append(subject);
+            }
+            catch(Exception ee)
+            {
+                Console.WriteLine("Error" + ee.ToString());
+            }
+
+            
+            Console.ReadKey();
+        }
+    }
+}
