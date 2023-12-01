b383dfda22ab51d536993f691669a50c0338b4b5
Akash Gautam <gautama55696@gmail.com>
2023-12-01 18:11:44 +0530

    C Sharp

---
+++ C#/Oops/Exception_Handling/Windows_Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_Square
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Student s=new Student();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            s.data(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox1.Text));
+            label4.Text = s.sum();
+            label5.Text = s.per();
+            label6.Text=s.GRADE();
+        }
+    }
+}
