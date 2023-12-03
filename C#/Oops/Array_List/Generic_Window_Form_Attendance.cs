cb55719b45668e85bab7489265b44a1f277e2fe1
Akash Gautam <gautama55696@gmail.com>
2023-12-03 20:59:07 +0530

    C Sharp

---
+++ C#/Oops/Array_List/Generic_Window_Form_Attendance.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Generic_WindowForm_Attendance
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        Dictionary<string, Student> d = new Dictionary<string, Student>();
+        Student s = null;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            s=new Student(textBox1.Text,Convert.ToInt32(textBox2.Text),textBox3.Text);
+            d.Add(s.name, s);
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox1.Focus();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+          
+            StringBuilder sb = new StringBuilder();
+            foreach(KeyValuePair<string,Student> kvp in d)
+            {
+               
+                sb.Append("Name: " + kvp.Key);
+                sb.Append("\nId: " + kvp.Value.id);
+                sb.Append("\nAttendance: " + kvp.Value.attend);
+                sb.Append("\n---------------------------------");
+            }   
+            label4.Text = sb.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int flagp = 0;
+            int flaga = 0;
+            StringBuilder sb = new StringBuilder();
+            foreach (KeyValuePair<string, Student> kvp in d)
+            {
+                if (kvp.Value.attend== "p")
+                {
+                    flagp++;
+                }
+                else if (kvp.Value.attend == "a")
+                {
+                    flaga++;
+                }
+               
+
+            }
+            sb.Append("\nPresent: " + flagp);
+            sb.Append("\nAbsent: " + flaga);
+
+
+            label5.Text = sb.ToString();
+        }
+    }
+}
---
+++ C#/Oops/Exception_Handling/Calculator_While.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class Calulator
+    {
+        public int n1;
+        public int n2;
+        public int result = 0;
+       
+        public void Add(int n1,int n2)
+        {
+             result = n1 + n2;
+           
+        }
+        public  void Sub(int n1, int n2)
+        {
+             result = n1 - n2;
+           
+        }
+        public void Div(int n1, int n2)
+        {
+             result = n1 / n2;
+           
+        }
+        public void Mul(int n1, int n2)
+        {
+             result = n1 * n2;
+           
+        }
+        public void display()
+        {
+            Console.WriteLine("Result is: " + result);
+            
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calulator c = new Calulator();
+            while (true)
+            {
+               
+                try
+                {
+                    Console.WriteLine("Enter Num1");
+                    int num1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter Num2");
+                    int num2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter Operator");
+                    string op = Console.ReadLine();
+
+                    if (op == "+")
+                    {
+                        c.Add(num1, num2);
+                        
+                    }
+                    else if (op == "-")
+                    {
+                        c.Sub(num1, num2);
+                     
+                    }
+                    else if (op == "/")
+                    {
+                        c.Div(num1, num2);
+
+                    }
+                    else if (op == "*")
+                    {
+                        c.Mul(num1, num2);
+                       
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Operator");
+                       
+                    }
+                    c.display();
+                    break;
+                    
+                }
+                catch (DivideByZeroException dd)
+                {
+                    Console.WriteLine("Does not divide");
+                    Console.WriteLine(dd.ToString());0
+                }
+                catch (Exception e) 
+                {
+                    Console.WriteLine("Does not allow string");
+                    Console.WriteLine(e.ToString());
+                    
+                }
+              
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
