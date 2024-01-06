9a70dc7da512a7353f12684289c5816eaf550a85
Akash Gautam <gautama55696@gmail.com>
2024-01-06 16:28:44 +0530

    Sql

---
+++ AdoNet/Student1 (2).cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    class Student
+    {
+      public int Rno { get; set; }
+      public string Name { get; set;}
+      public string City { get; set; }
+      public string Gender { get; set; }
+      public string Hobby { get; set; }
+      public string Date {  get; set; }
+      public Student(int Rno,string Name,string City,string Gender,string Hobby,string Date)
+      {
+            this.Rno = Rno;
+            this.Name = Name;
+            this.City = City;
+            this.Gender = Gender;
+            this.Hobby = Hobby;
+            this.Date = Date;
+      }
+
+
+    }
+}
---
+++ Sql/Student_Ado.net.sql
@@ -0,0 +1,3 @@
+CREATE TABLE Student(Rno int,Name varchar(20),City varchar(20),Gender varchar(20),Hobby varchar(20),Date varchar(20))
+
+select * from Student;
